08c548efc6998ca7389c1fe388b4aa681bb4b22c
Apostolos Dimoulakis <apo.dimu@gmail.com>
2025-03-09 22:48:51 +0200

    Contrastive loss function PyTorch implementation

--- src/engine/optimizer.py
+++ src/engine/optimizer.py
@@ -1,8 +1,7 @@
-from numpy.typing import NDArray
-import numpy as np
+import torch
 
 
-def contrastive_loss(projected_pairs: tuple[NDArray], temperature: float) -> float:
+def contrastive_loss(projected_pairs: tuple[torch.Tensor], temperature: float) -> torch.Tensor:
   '''
   Description:
     Computation of the NT-Xent loss function for a set of input projected pairs.
@@ -12,28 +11,30 @@ def contrastive_loss(projected_pairs: tuple[NDArray], temperature: float) -> flo
     `temperature`. Temperature parameter.
 
   Returns:
-    `loss_value`.
+    `loss`. Scalar.
   '''
 
-  M, _, Np = projected_pairs.shape
-  projections = np.reshape(projected_pairs, shape=(2*M, Np)) # projections[2*p], projections[2*p+1] -> projected_pairs[p]
+  M, two, Np = projected_pairs.shape
+  assert two == 2, 'E: This should be a matrix containing a pair of representations in the second axis. Instead the second axis of the received tensor does not contain a pair.'
+
+  projections = torch.reshape(input=projected_pairs, shape=(2*M, Np)) # projections[2*p], projections[2*p+1] -> projected_pairs[p]
   sim_mat = similarity(mat=projections) # Shape (2*M, 2*M)
-  exp_sim_mat = np.exp(sim_mat / temperature) # Shape (2*M, 2*M)
+  exp_sim_mat = torch.exp(sim_mat / temperature) # Shape (2*M, 2*M)
 
   # Removal of the self similarity from ell's denominator; the numerator does not use self similarity either hence no conflicts emerge from this
-  np.fill_diagonal(a=exp_sim_mat, val=0)
+  exp_sim_mat.fill_diagonal_(fill_value=0)
 
-  first_idx_of_flattened_pairs = np.arange(2 * M)
-  second_idx_of_flattened_pairs = np.array([i+1 if i % 2 == 0 else i-1 for i in range(2 * M)])
+  first_idx_of_flattened_pairs = torch.arange(2 * M)
+  second_idx_of_flattened_pairs = torch.tensor([i+1 if i % 2 == 0 else i-1 for i in range(2 * M)])
   numerators = exp_sim_mat[first_idx_of_flattened_pairs, second_idx_of_flattened_pairs]
-  denominators = np.sum(a=exp_sim_mat, axis=1)
-  losses = -np.log(numerators / (denominators + 1e-10))
+  denominators = torch.sum(a=exp_sim_mat, axis=1)
+  losses = -torch.log(numerators / (denominators + 1e-10))
 
-  loss = np.mean(a=losses, axis=0)
+  loss = torch.mean(a=losses, axis=0)
 
   return loss
 
-def similarity(mat: NDArray) -> NDArray:
+def similarity(mat: torch.Tensor) -> torch.Tensor:
   '''
   Description:
     Cosine similarity between vector pairs in a Euclidean vector space equipped with the Euclidean inner product.
@@ -45,11 +46,8 @@ def similarity(mat: NDArray) -> NDArray:
     `sim_mat`. Shape (m, m). Similarity matrix.
   '''
 
-  norm2 = np.linalg.norm(x=mat, ord=2, axis=1, keepdims=True) # Shape (m, 1)
+  norm2 = torch.norm(input=mat, p=2, dim=1, keepdim=True) # Shape (m, 1)
   mat_normalized = mat / (norm2 + 1e-10)
-  sim_mat = np.matmul(mat_normalized, mat_normalized.T)
+  sim_mat = torch.matmul(input=mat_normalized, other=mat_normalized.T)
 
   return sim_mat
-
-
-
